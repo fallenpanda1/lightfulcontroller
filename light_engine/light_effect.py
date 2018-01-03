b9246446e4553d3a0d18cd75f1c70c5b028c4089
Allen Pan <allenpan@Allens-MacBook-Pro.local>
2018-01-03 00:20:12 -0800

    Introduce a repeating task and a gradient effect.

--- light_engine/light_effect.py
+++ light_engine/light_effect.py
@@ -2,19 +2,26 @@ import time
 from scheduler.scheduler import Task
 import logging
 from abc import ABC, abstractmethod
+import operator
+import math
 
 logger = logging.getLogger("global")
 
 class LightSection:
     def __init__(self, positions):
         self.positions = positions
+        length = len(positions)
 
-    def gradient(self, index):
-        return 0
+        # create a list ranging from 0 to 1
+        # todo: make this customizable later
+        self.gradients = list(map(operator.truediv, list(range(length)), [length] * length))
+
+    def __str__(self):
+        return str(self.positions)
 
 class LightEffect:
     @abstractmethod
-    def get_color(self, progress, position):
+    def get_color(self, progress, gradient):
         """Called for each light effect task tick"""
         pass
 
@@ -24,8 +31,19 @@ class SolidColorLightEffect(LightEffect):
     def __init__(self, color):
         self.color = color
 
-    def get_color(self, progress, position):
-        return self.color.with_alpha(1 - progress)
+    def get_color(self, progress, gradient):
+        return self.color.with_alpha(max(0, 1 - progress))
+
+class GradientLightEffect(LightEffect):
+    """Light effect that applies gradient over time to light section"""
+
+    def __init__(self, color1, color2):
+        self.color1 = color1
+        self.color2 = color2
+
+    def get_color(self, progress, gradient):
+        alpha = math.sin((progress - gradient) * math.pi * 2) / 2 + 0.5
+        return self.color1.with_alpha(alpha).blended_with(self.color2)
 
 class LightEffectTask(Task):
     """An effect task contains a light effect and all the state around animating it.
@@ -42,8 +60,9 @@ class LightEffectTask(Task):
         self._start_time = time.time() # protected
 
     def tick(self):
-        for position in self.section.positions:
-            new_color = self.effect.get_color(self.progress(), position)
+        for index, position in enumerate(self.section.positions):
+            gradient = self.section.gradients[index]
+            new_color = self.effect.get_color(self.progress(), gradient)
             existing_color = self.light_adapter.get_color(position)
             self.light_adapter.set_color(position, new_color.blended_with(existing_color))
 
@@ -53,6 +72,26 @@ class LightEffectTask(Task):
     def is_finished(self):
         return self.progress() == 1
 
+class RepeatingTask(Task):
+    def __init__(self, task):
+        self.__repeating = True
+        self.task = task
+
+    def stop_repeating(self):
+        self.__repeating = False
+
+    def tick(self):
+        self.task.tick()
+
+    def progress(self):
+        return self.task.progress()
+
+    def is_finished(self):
+        if self.__repeating and self.task.is_finished():
+            self.task._start_time = time.time()
+            return False
+
+        return self.task.is_finished()
 
 # TODO: definitely goes elsewhere
 class MidiOffLightEffectTask(LightEffectTask):
