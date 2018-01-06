b90b21c85759cce6c75ee613514b757787640c5a
Allen Pan <allenpan@Allens-MacBook-Pro.local>
2018-01-06 12:21:45 -0800

    include velocity parameter for light effects.

--- light_engine/light_effect.py
+++ light_engine/light_effect.py
@@ -19,9 +19,12 @@ class LightSection:
     def __str__(self):
         return str(self.positions)
 
+    def reversed(self):
+        return LightSection(list(reversed(self.positions)))
+
 class LightEffect:
     @abstractmethod
-    def get_color(self, progress, gradient):
+    def get_color(self, progress, gradient, velocity):
         """Called for each light effect task tick"""
         pass
 
@@ -31,7 +34,7 @@ class SolidColorLightEffect(LightEffect):
     def __init__(self, color):
         self.color = color
 
-    def get_color(self, progress, gradient):
+    def get_color(self, progress, gradient, velocity):
         return self.color.with_alpha(max(0, 1 - progress))
 
 class GradientLightEffect(LightEffect):
@@ -41,9 +44,27 @@ class GradientLightEffect(LightEffect):
         self.color1 = color1
         self.color2 = color2
 
-    def get_color(self, progress, gradient):
+    def get_color(self, progress, gradient, velocity):
         alpha = math.sin((progress - gradient / 2) * math.pi * 2) / 2 + 0.5
         return self.color1.with_alpha(alpha).blended_with(self.color2)
+
+class MeteorLightEffect(LightEffect):
+    def __init__(self, color, tail_length=1):
+        self.color = color
+        self.tail_length = tail_length # 1 provides a 'standard' length, 0.5 will halve, 2 will double
+
+    def get_color(self, progress, gradient, velocity):
+        if 0.0 < gradient - progress < 0.1:
+            alpha = abs((gradient - progress) * 10)
+            return self.color.with_alpha(alpha)
+        else:
+            return self.color.with_alpha(0)
+             # since we have to animate starting from off the screen and then go off the screen,
+             # progress should range from -1 to 2
+            #progress *= 3
+            #progress -= 1
+            
+            #return self.color.with_alpha(alpha)
 
 class LightEffectTask(Task):
     """An effect task contains a light effect and all the state around animating it.
@@ -63,9 +84,10 @@ class LightEffectTask(Task):
         self._start_time = time.time()
 
     def tick(self):
+        velocity = 1.0 * len(self.section.positions) / self.duration
         for index, position in enumerate(self.section.positions):
             gradient = self.section.gradients[index]
-            new_color = self.effect.get_color(self.progress(), gradient)
+            new_color = self.effect.get_color(self.progress(), gradient, velocity)
             existing_color = self.light_adapter.get_color(position)
             self.light_adapter.set_color(position, new_color.blended_with(existing_color))
 
@@ -108,11 +130,11 @@ class MidiOffLightEffectTask(LightEffectTask):
         self.task = task
         self.pitch = pitch
         self.__midi_monitor = midi_monitor
-        midi_monitor.register(self)
-        self.__note_off_received = False
 
     def start(self):
         self.task.start()
+        self.__midi_monitor.register(self)
+        self.__note_off_received = False
 
     def received_midi(self, rtmidi_message):
         if rtmidi_message.isNoteOff() and rtmidi_message.getNoteNumber() == self.pitch:
